b336564c2703547c8882aac89dbe22831ad3eec1
Jeong hyunyong <jhy5317@gmail.com>
2026-07-10 11:26:40 +0900

    260710 1126

--- app.py
+++ app.py
@@ -12,17 +12,20 @@ def search():
     keyword = request.args.get("keyword")
     print(keyword)
 
-    incruit = search_incruit(keyword)
+    incruit = search_incruit(keyword, 2)
     print(incruit)
-    hrd24 = search_hrd24(keyword)
+    hrd24 = search_hrd24(keyword, 2)
     print(hrd24)
-    return render_template("search.html", jobs1 = enumerate(incruit), jobs2 = enumerate(hrd24)) # html에서 enumerate를 사용할 수 없어서 미리 묶어서 보내기
+    return render_template("search.html", jobs1 = enumerate(incruit), jobs2 = enumerate(hrd24), keyword = keyword, count = (len(incruit) + len(hrd24))) # html에서 enumerate를 사용할 수 없어서 미리 묶어서 보내기
+
+@app.route("/file")
+def download_csv():
+    return "file"
 
 
 # @app.route('/hello')
 # def hello():
 #     return "안녕하세요"
-
 
 if __name__ == "__main__":
     app.run(debug = True)
--- scrapper.py
+++ scrapper.py
@@ -1,67 +1,87 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Whale/4.38.386.14 Safari/537.36"
-}
+# headers = {
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Whale/4.38.386.14 Safari/537.36"
+# }
 
 # r = requests.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
 # print(r.status_code)
 
-def search_incruit(keyword):
+def search_incruit(keyword, page = 1):
 
-    # 인크루트
-    url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}"
-    r = requests.get(url)
-    # print(r.text)
-
-    soup = BeautifulSoup(r.text, "html.parser")
-    lis = soup.find_all("li", class_ = "c_col")
+    pages = 30 * (page - 1)
 
     incruit_jobs = []
 
-    for li in lis:
-        company = li.find("a", class_ = "cpname").text
-        title = li.find("div", class_ = "cell_mid").find("div", class_ = 'cl_top').find("a").text
-        location = li.find("div", class_ = "cl_md").find_all("span")[0].text
-        link = li.find("div", class_ = "cell_mid").find("div", class_ = 'cl_top').find("a").get("href")
+    for i in range(page):
+    # 인크루트
+        page = 30 * i
+        url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno={page}"
+        r = requests.get(url)
+        # print(r.text)
 
-        job_data = {
-            "company": company,
-            "title": title,
-            "location": location,
-            "link": link
-        }
+        soup = BeautifulSoup(r.text, "html.parser")
+        lis = soup.find_all("li", class_ = "c_col")
 
-        incruit_jobs.append(job_data)
+        for li in lis:
+            company = li.find("a", class_ = "cpname").text
+            title = li.find("div", class_ = "cell_mid").find("div", class_ = 'cl_top').find("a").text
+            location = li.find("div", class_ = "cl_md").find_all("span")[0].text
+            link = li.find("div", class_ = "cell_mid").find("div", class_ = 'cl_top').find("a").get("href")
+
+            job_data = {
+                "company": company,
+                "title": title,
+                "location": location,
+                "link": link
+            }
+
+            incruit_jobs.append(job_data)
 
     return incruit_jobs
 
-def search_hrd24(keyword):
+if __name__ == '__main__': 
+    result = search_incruit("간호사", 2)
+    print(result)
+    print(len(result))
 
-    # 고용24
-    url = f"https://www.work24.go.kr/cm/f/c/0100/selectUnifySearch.do?topQuerySearchArea=tb_workinfo&topQueryData={keyword}"
-    r = requests.get(url)
-    # print(r.text)
-
-    soup = BeautifulSoup(r.text, "html.parser")
-    lis = soup.find("ul", class_ = "srch_list_default").find_all("li")
+def search_hrd24(keyword, page = 1):
 
     hrd24_jobs = []
 
-    for li in lis:
-        company = li.find("dl", class_ = "dl_list").find("strong", class_ = "b1_sb").text
-        title = li.find("dd").find("a").text.strip()
-        location = li.find("div", class_ = "vline_group type2 small").find_all("span", class_ = "item")[4].text
-        link = li.find("dd").find("a").get("href")
+    for i in range(1, page + 1):
+        # 고용24
+        url = f"https://www.work24.go.kr/cm/f/c/0100/selectUnifySearch.do?topQuerySearchArea=tb_workinfo&topQueryData={keyword}&startCount={i}"
+        r = requests.get(url)
+        # print(r.text)
 
-        job_data = {
-            "company": company,
-            "title": title,
-            "location": location,
-            "link": link
-        }
+        soup = BeautifulSoup(r.text, "html.parser")
+        lis = soup.find("ul", class_ = "srch_list_default").find_all("li")
 
-        hrd24_jobs.append(job_data)
+        for li in lis:
+            company = li.find("dl", class_ = "dl_list").find("strong", class_ = "b1_sb").text
+            title = li.find("dd").find("a").text.strip()
+            location = li.find("div", class_ = "vline_group type2 small").find_all("span", class_ = "item")[4].text.strip("근무지 ")
+            link = li.find("dd").find("a").get("href")
+
+            job_data = {
+                "company": company,
+                "title": title,
+                "location": location,
+                "link": link
+            }
+
+            hrd24_jobs.append(job_data)
 
     return hrd24_jobs
+
+
+# url = "https://www.saramin.co.kr/zf_user/search?search_area=main&search_done=y&search_optional_item=n&searchType=search&searchword=%EA%B0%84%ED%98%B8%EC%82%AC"
+# r = requests.get(url)
+# print(r)
+
+# soup = BeautifulSoup(r.text, "html.parser")
+# lis = soup.find("div", {"id":"recruit_info_list"})#.find_all("div", class_ = "item_recruit")
+
+# print(lis)
